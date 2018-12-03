3353edd9f40bfb3ed82452d603ab0d19fb049120
serbernari <hirugas7@gmail.com>
2018-12-03 13:19:52 +0300

    working tests

--- Structures and types of data/CheckersCheck/Sample-Test1/test.cpp
+++ Structures and types of data/CheckersCheck/Sample-Test1/test.cpp
@@ -1,14 +1,27 @@
 #include "gtest/gtest.h"
-#include "../CheckersCheck/checkers.h"
+#include "../CheckersCheck/checkers.cpp"
 
-TEST(CorrectInput, EmptyField) {
-
-	board MyBoard;
-	bool a = possibilityToMove(&MyBoard);
-    EXPECT_FALSE(a);
-}
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
+}
+
+TEST(CorrectInput, EmptyField) {
+	board MyBoard;
+    EXPECT_FALSE(possibilityToMove(&MyBoard));
+}
+
+TEST(CorrectInput, White_on_the_top) {
+	board MyBoard;
+	MyBoard.field[7][0].is_buisy = true;
+	MyBoard.field[7][0].is_white = true;
+	EXPECT_FALSE(possibilityToMove(&MyBoard));
+}
+
+TEST(CorrectInput, White_near_the_top) {
+	board MyBoard;
+	MyBoard.field[6][0].is_buisy = true;
+	MyBoard.field[6][0].is_white = true;
+	EXPECT_TRUE(possibilityToMove(&MyBoard));
 }
